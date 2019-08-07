ebf7003ae9e1d176a07cbe00f955632b6b4afb43
ryosuke-akashi <ryosuke.akashi31@gmail.com>
2019-08-07 17:46:27 +0900

    Removed unneccesary files in analytic_pot

--- analytic_pot/ONLY_FORCES/val_mpi_lmp.f90
+++
@@ -1,8 +0,0 @@
-module val_mpi
-  implicit none
-  integer :: ierr, myrank, nprocs
-end module val_mpi
-module val_mpi_lmp
-  implicit none
-  integer :: lammps, comm_lammps, ptr
-end module val_mpi_lmp
